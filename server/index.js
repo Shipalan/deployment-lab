40d2e7e62792adcfb5773d0aede58af4b509e8b3
Shipalan <Shipalan678@gmail.com>
2022-06-29 14:21:26 -0600

    correcting app.get issues

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const path = require('path')
 
 const app = express()
 
-app.get('/js', (req,res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/js', (req,res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
 })
 
 
