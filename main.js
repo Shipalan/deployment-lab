c0ac29f8b392b61899ae8f2b6913360c3b6d8f8b
Shipalan <Shipalan678@gmail.com>
2022-06-29 16:02:33 -0600

    final product4

---
+++ main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#button')
+
+const buyNow = function() {
+    window.location='https://pourri.com/products/heavy-doody'
+}
+
+button.addEventListener('click', buyNow)
