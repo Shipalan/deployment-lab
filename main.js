7dfbf11f52bf4c835aa263fe7a475b72603e5c01
Shipalan <Shipalan678@gmail.com>
2022-06-30 15:31:03 -0600

    trying to add a broken function

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const button = document.querySelector('#button')
 
+const baseUrl = 'https://f24-deployment-lab.herokuapp.com/'
+
 const buyNow = function() {
     window.location='https://pourri.com/products/heavy-doody'
+}
+
+const brokenFunction = () => {
+axios.get(baseUrl + '/api/brokenFunction/')
+.then(res => {
+    const data = res.data;
+    alert(data);
+}) .catch((err) => {
+    
+})
 }
 
 button.addEventListener('click', buyNow)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '5be130e87b724611b961bb6ce570faca',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 
 const app = express()
 
@@ -16,7 +27,10 @@ app.get('/css', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.css'))
 })
 
-
+app.get('/brokenFunction', (req,res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+    
+})
 
 const port = process.env.PORT || 4005
 
